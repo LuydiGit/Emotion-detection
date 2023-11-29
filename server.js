92c4d896d1cbe673552e7784c52a08b8f68735a1
Luydi Bentes <81865020+LuydiGit@users.noreply.github.com>
2023-11-29 17:19:51 -0300

    Status code

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ app.post("/signup", (req, res) => {
   db.query(sql, [name, email, password], (err, result) => {
     if (err) {
       console.error("Erro na inserção no banco de dados:", err);
-      res.json({ success: false });
+      res.status(500).json({ success: false }); // Internal Server Error
       return;
     }
     console.log("Cadastro realizado com sucesso!");
-    res.json({ success: true });
+    res.status(200).json({ success: true }); // OK
   });
 });
 
@@ -48,7 +48,7 @@ app.post("/login", (req, res) => {
   db.query(sql, [name, password], (err, result) => {
       if (err) {
           console.error("Erro na consulta ao banco de dados:", err);
-          res.json({ success: false });
+          res.status(500).json({ success: false }); // Internal Server Error
           return;
       }
 
@@ -56,51 +56,51 @@ app.post("/login", (req, res) => {
       if (result.length > 0) {
           const user = result[0];
           console.log(`Usuário logado: ${user.name}`);
-          res.json({ success: true, user: user });
+          res.status(200).json({ success: true, user: user }); // OK
       } else {
-          res.json({ success: false });
+          res.status(401).json({ success: false }); // Unauthorized
       }
   });
+});
 
-  app.post("/consultarEmotions", (req, res) => {
-    const { userid } = req.body;
-  
-    // Consultar o banco de dados para verificar as credenciais
-    const sql = "SELECT * FROM emotion WHERE user_id=?";
-    db.query(sql, [userid], (err, result) => {
-      if (err) {
-        console.error("Erro na consulta ao banco de dados:", err);
-        res.json({ success: false });
-        return;
-      }
-  
-      // Verificar se há um usuário correspondente
-      if (result.length > 0) {
-        const emotion = result[0];
-        res.json({ success: true, emotion: result });
-       console.log (result);
-      } else {
-        res.json({ success: false });
-      }
-    });
+// Rota para consultar emoções no banco de dados
+app.post("/consultarEmotions", (req, res) => {
+  const { userid } = req.body;
+
+  // Consultar o banco de dados para verificar as credenciais
+  const sql = "SELECT * FROM emotion WHERE user_id=?";
+  db.query(sql, [userid], (err, result) => {
+    if (err) {
+      console.error("Erro na consulta ao banco de dados:", err);
+      res.status(500).json({ success: false }); // Internal Server Error
+      return;
+    }
+
+    // Verificar se há um usuário correspondente
+    if (result.length > 0) {
+      const emotion = result[0];
+      res.status(200).json({ success: true, emotion: result }); // OK
+      console.log(result);
+    } else {
+      res.status(404).json({ success: false }); // Not Found
+    }
   });
+});
 
 // Rota para salvar emoções no banco de dados
 app.post("/saveEmotions", (req, res) => {
-    const { timestamp, emotion, user_id } = req.body;
-  
-    const sql = "INSERT INTO emotion (timestamp, emocao, user_id) VALUES (?, ?, ?)";
-    db.query(sql, [timestamp, emotion, user_id], (err, result) => {
-      if (err) {
-        console.error("Erro na inserção no banco de dados:", err);
-        res.json({ success: false });
-        return;
-      } else {
-      console.log("Emoção salva no banco de dados com sucesso!");
-      res.json({ success: true });
-      }
-    });
-});
+  const { timestamp, emotion, user_id } = req.body;
+
+  const sql = "INSERT INTO emotion (timestamp, emocao, user_id) VALUES (?, ?, ?)";
+  db.query(sql, [timestamp, emotion, user_id], (err, result) => {
+    if (err) {
+      console.error("Erro na inserção no banco de dados:", err);
+      res.status(500).json({ success: false }); // Internal Server Error
+      return;
+    }
+    console.log("Emoção salva no banco de dados com sucesso!");
+    res.status(200).json({ success: true }); // OK
+  });
 });
 
 app.listen(port, () => {
