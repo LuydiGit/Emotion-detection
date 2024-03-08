5800c954fccc75f041048656646dd9e459534513
Luydi Bentes <81865020+LuydiGit@users.noreply.github.com>
2024-03-08 00:55:39 -0300

    Virgula errada na criação do banco

--- emotiondetection.sql
+++ emotiondetection.sql
@@ -14,11 +14,11 @@ CREATE TABLE `user` (
 CREATE TABLE `patient` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `consultation` (
   `id` int(11) NOT NULL,
   `notes` varchar(255) NOT NULL,
-  `consultation_datetime` DATETIME,
+  `consultation_datetime` DATETIME
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
